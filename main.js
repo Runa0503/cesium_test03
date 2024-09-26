1ad11cfacda341c73c04092ea57ecf1f4f6b7b8e
Runa0503 <91305683+Runa0503@users.noreply.github.com>
2024-09-26 20:07:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -50,22 +50,6 @@ viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(139.63148565, 35.4545858, 10000.0),
 });
 
-// 3D Tilesデータが読み込まれた後の処理
-Promise.all(tilesets.map(tileset => tileset.readyPromise)).then(function() {
-    viewer.zoomTo(tilesets);
-
-    // マウスクリックイベントの設定
-    var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
-    handler.setInputAction(function(movement) {
-        var pickedObject = viewer.scene.pick(movement.position);
-        if (Cesium.defined(pickedObject) && pickedObject.id) {
-            viewer.selectedEntity = pickedObject.id;
-        }
-    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
-}).otherwise(function(error) {
-    console.error(error);
-});
-
 
 // エンティティの追加
 viewer.entities.add({
@@ -94,4 +78,113 @@ viewer.entities.add({
         disableDepthTestDistance: 1000000.0,
         heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
     }
+});
+
+// JSONファイルを読み込み
+fetch('yokohama_restaurant_1000_1km_787.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            const reviewsHtml = item.reviews.map(review => `
+                <tr>
+                    <td><strong>${review.author_name}</strong></td>
+                    <td>${review.rating} stars</td>
+                    <td>${review.text}</td>
+                    <td>${review.relative_time_description}</td>
+                </tr>
+            `).join('');
+
+            const openingHoursHtml = item.opening_hours.map(hour => `
+                <tr>
+                    <td>${hour}</td>
+                </tr>
+            `).join('');
+
+            const description = `
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <td>${item.name}</td>
+                    </tr>
+                    <tr>
+                        <th>Location</th>
+                        <td>${item.geometry.location.lat}, ${item.geometry.location.lng}</td>
+                    </tr>
+                    <tr>
+                        <th>Types</th>
+                        <td>${item.types.join(', ')}</td>
+                    </tr>
+                    <tr>
+                        <th>Address</th>
+                        <td>${item.formatted_address}</td>
+                    </tr>
+                    <tr>
+                        <th>Phone Number</th>
+                        <td>${item.formatted_phone_number}</td>
+                    </tr>
+                    <tr>
+                        <th>Website</th>
+                        <td><a href="${item.website}" target="_blank">${item.website}</a></td>
+                    </tr>
+                    <tr>
+                        <th>Price Level</th>
+                        <td>${item.price_level}</td>
+                    </tr>
+                    <tr>
+                        <th>Rating</th>
+                        <td>${item.rating}</td>
+                    </tr>
+                    <tr>
+                        <th>User Ratings Total</th>
+                        <td>${item.user_ratings_total}</td>
+                    </tr>
+                    <tr>
+                        <th>Opening Hours</th>
+                        <td>
+                            <table>${openingHoursHtml}</table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Reviews</th>
+                        <td>
+                            <table>${reviewsHtml}</table>
+                        </td>
+                    </tr>
+                </table>
+            `;
+
+            viewer.entities.add({
+                name: item.name,
+                description: description,
+                position: Cesium.Cartesian3.fromDegrees(item.geometry.location.lng, item.geometry.location.lat, 0),
+                point: {
+                    pixelSize: 20,
+                    color: Cesium.Color.BLUE,
+                    eyeOffset: new Cesium.Cartesian3(0.0, 0.0, -5.0),
+                    distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0.0, 1000000.0),
+                    disableDepthTestDistance: 1000000.0,
+                    heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
+                }
+            });
+        });
+    })
+    .catch(error => {
+        console.error('JSONファイルの読み込みエラー:', error);
+    });
+
+
+// 3D Tilesデータが読み込まれた後の処理
+Promise.all(tilesets.map(tileset => tileset.readyPromise)).then(function() {
+    viewer.zoomTo(tilesets);
+
+    // マウスクリックイベントの設定
+    var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+    handler.setInputAction(function(movement) {
+        var pickedObject = viewer.scene.pick(movement.position);
+        if (Cesium.defined(pickedObject) && pickedObject.id) {
+            viewer.selectedEntity = pickedObject.id;
+        }
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+}).otherwise(function(error) {
+    console.error(error);
 });
