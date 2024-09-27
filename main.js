fd73367467be3d39a0fbf2593a0dc44288a2ab52
Runa0503 <91305683+Runa0503@users.noreply.github.com>
2024-09-27 12:21:39 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Cesium ionのアクセストークン（公式に記載があったものを使用しています）
+// Cesium ionのアクセストークン
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5N2UyMjcwOS00MDY1LTQxYjEtYjZjMy00YTU0ZTg5MmViYWQiLCJpZCI6ODAzMDYsImlhdCI6MTY0Mjc0ODI2MX0.dkwAL1CcljUV7NA7fDbhXXnmyZQU_c-G5zRx8PtEcxE';
 
 // Cesium ViewerをcesiumContainerというIDのHTML要素に初期化
@@ -16,46 +16,22 @@ var imageProvider = new Cesium.UrlTemplateImageryProvider({
 });
 var current_image = viewer.scene.imageryLayers.addImageryProvider(imageProvider);
 
-// 横浜市の全部の区の3D TilesデータのURL
-var tilesetUrls = [
-    'https://assets.cms.plateau.reearth.io/assets/71/e76732-4a56-4654-b0f4-c00b8d502f7a/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14103_nishi-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/8f/4d287c-71b4-4044-aec2-0164e407a845/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14104_minami-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/84/5b5e7e-d4bc-41b0-9b6a-74107b87ec0f/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14105_konanku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/7c/5a91e5-58a5-4e7b-bd17-2b4d67a8dd7d/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14106_hodogaya-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/75/cbb432-61d5-4c7b-9dc5-47c0427d1c12/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14107_asahi-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/05/fc39b5-3a6b-44e2-98ef-cb8994e3082f/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14108_kanagawa-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/97/3443a0-146e-4e7e-8e2d-052f5c292405/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14109_tsurumi-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/5f/9c6ae0-96c7-401e-b77b-0d4a939fc3c5/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14110_kohoku-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/cb/2a241c-d78d-4d26-bb1b-f1be62129cf1/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14111_midori-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/6a/994ed8-66e7-41a7-8d6b-e60d02a88d8a/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14112_aoba-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/94/1dc750-c71f-41ec-b3a8-7691a017a09b/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14113_izumi-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/3b/02b20d-8c39-48a4-ace8-b7d94a450502/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14114_sakae-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/7e/b61395-071d-4670-8d82-7a1af2d3b1e8/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14115_totsuka-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/4f/78752b-40be-4cc5-871a-16df4e77a31e/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14116_kamigyo-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/05/66ad07-9d70-420b-9b69-04dbdd2392e8/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14117_tamotsu-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/3e/003923-6c72-4e4d-9a88-9f1c7b8d2999/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14118_minato-ku_lod1/tileset.json',
-    'https://assets.cms.plateau.reearth.io/assets/bc/1d4095-3c55-44a4-8c1b-3a2fcd45a17e/14100_yokohama-shi_city_2023_citygml_1_op_bldg_3dtiles_14119_seya-ku_lod1/tileset.json'
-];
-
-var tilesets = [];
-
-tilesetUrls.forEach(function(url) {
-    var tileset = new Cesium.Cesium3DTileset({ url: url });
-    viewer.scene.primitives.add(tileset);
-    tilesets.push(tileset);
-});
-
+// 神奈川県横浜市の建物データ（3D Tiles）
+var your_3d_tiles = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+        url: 'https://plateau.geospatial.jp/main/data/3d-tiles/bldg/14100_yokohama/low_resolution/tileset.json',
+    })
+);
 // 初期表示時のカメラ位置
 viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(139.63148565, 35.4545858, 10000.0),
 });
 
-
 // エンティティの追加
 viewer.entities.add({
-    name: "横浜駅",
-    description: "〒220-0011 神奈川県横浜市西区高島２丁目",
-    position: Cesium.Cartesian3.fromDegrees(139.6221073371378,35.4661238909113, 0),
+    name: "町田駅",
+    description: "東京都町田市原町田６丁目１２−２０",
+    position: Cesium.Cartesian3.fromDegrees(139.44536536590812,35.541994072641195, 0),
     point: {
         pixelSize: 20,
         color: Cesium.Color.RED,
@@ -67,9 +43,9 @@ viewer.entities.add({
 });
 
 viewer.entities.add({
-    name: "横浜検索の中心",
-    description: "",
-    position: Cesium.Cartesian3.fromDegrees(139.474,35.373,0),
+    name: "町田検索の中心",
+    description: "日本、〒221-0825 神奈川県横浜市神奈川区反町１丁目５−１２ シティコープ反町",
+    position: Cesium.Cartesian3.fromDegrees(139.446,35.541,0),
     point: {
         pixelSize: 20,
         color: Cesium.Color.RED,
@@ -81,7 +57,7 @@ viewer.entities.add({
 });
 
 // JSONファイルを読み込み
-fetch('yokohama_restaurant_1000_1km_840.json')
+fetch('machida_restaurant_2km_1000_867.json')
     .then(response => response.json())
     .then(data => {
         data.forEach(item => {
@@ -184,7 +160,6 @@ Promise.all(tilesets.map(tileset => tileset.readyPromise)).then(function() {
             viewer.selectedEntity = pickedObject.id;
         }
     }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
-    
 }).otherwise(function(error) {
     console.error(error);
 });
